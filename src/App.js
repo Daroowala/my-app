e4f4fb50489bbe3d5e04f28f987e429246e0012a
Daroowala <153667031+Daroowala@users.noreply.github.com>
2024-10-04 02:46:36 +0500

    testing phase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name = "Rehan"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Rehan
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Contact Us</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, et obcaecati! Voluptatum autem excepturi quas voluptates sunt quibusdam fugit itaque doloremque architecto, dolore repudiandae rerum, quod beatae voluptas expedita id tempora dolorum natus aspernatur.
+      </p>
+      
     </div>
+    </>
   );
 }
 
